03b139186dd3f826567a199521bafe7f831da756
Dave Smith <dsmith@rwbaird.com>
2021-09-02 09:45:02 -0500

    Empty projects

---
+++ coin-changer.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace coin_changer.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ coin-changer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coin_changer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
